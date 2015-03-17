c83f387acb90454f8353068eb701e3fd2c317a7e
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2015-03-17 01:44:07 -0700

    js work

--- assets/js/scripts/src/general.js
+++ assets/js/scripts/src/general.js
@@ -13,30 +13,28 @@ $(function(){
 				$('.' + gameNumber + ' input:checked').each(function(){
 					squad.push($(this).val());
 				});
+
+				function getMessage() {
+	   				return squad[Math.floor(Math.random() * squad.length)];
+				}
+
+				function squadImg(member) {
+					var selected = $('.member' + member +' .squad-name').text();
+					var img = $('.members input[value="' + selected + '"]').attr('data-img');
+					$('.member' + member +' img').attr('src','assets/img/' + img + '.png');
+				}
+
+				$('.member1 .squad-name').text(getMessage());
+				squadImg('1');
+
+				var removeItem = $('.member1 .squad-name').text();
+				squad = jQuery.grep(squad, function(value) {
+				  return value !== removeItem;
+				});
+
+				$('.member2 .squad-name').text(getMessage());
+				squadImg('2');
 			}
-
-			function getMessage() {
-   				return squad[Math.floor(Math.random() * squad.length)];
-			}
-
-			function squadImg(member) {
-				var selected = $('.member' + member +' .squad-name').text();
-				var img = $('.members input[value="' + selected + '"]').attr('data-img');
-				$('.member' + member +' img').attr('src','assets/img/' + img + '.png');
-			}
-
-			$('.member1 .squad-name').text(getMessage());
-			squadImg('1');
-			//console.log(squad);
-
-			var removeItem = $('.member1 .squad-name').text();
-			squad = jQuery.grep(squad, function(value) {
-			  return value !== removeItem;
-			});
-
-			$('.member2 .squad-name').text(getMessage());
-			squadImg('2');
-			//console.log(squad);
 		}
 
 		sqaudSelect('me1');
