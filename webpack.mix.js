96b6bf5eca1c8e833e11b88b2d2499477af3e8d3
geoffreyrose <geoffrey@geoffreyrose.io>
2022-08-22 18:39:56 -0700

    new design and mass effect andromeda squad

---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+let mix = require('laravel-mix');
+
+mix.setPublicPath('public')
+    .js('src/js/app.js', 'public/assets/js/app.js')
+    .sass('src/sass/app.scss', 'public/assets/css/app.css')
+    .copy('src/images', 'public/assets/images')
+    .browserSync('squad-randomizer.test')
